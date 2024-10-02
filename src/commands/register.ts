27aaa5eb684301a475051ce670148a12e3a96f6a
Sérgio Seco <52105707+SurgeCode@users.noreply.github.com>
2024-10-02 12:13:34 +0100

    Minor fixes (#3)
    * Comment out unused commands
    
    * Log

--- src/commands/register.ts
+++ src/commands/register.ts
@@ -1,20 +1,19 @@
-import { Command } from "commander";
-import { registerPlugin } from "../services/plugin-service";
-import { openPlayground } from "../services/tunnel-service";
-import { getAccountId } from "../utils/url-utils";
+// import { Command } from "commander";
+// import { registerPlugin } from "../services/plugin-service";
+// import { openPlayground } from "../services/tunnel-service";
 
-export const registerCommand = new Command()
-    .name('register')
-    .description('Register a new plugin with a URL')
-    .argument('<url>', 'URL of the plugin')
-    .action(async (url) => {
-        const pluginId = new URL(url).hostname;
-        const accountId = await getAccountId(url)
-        const result = await registerPlugin(pluginId, accountId);
-        if (result) {
-            const receivedId = await openPlayground(result);
-            console.log(`Received ID from playground: ${receivedId}`);
-        } else {
-            console.log('Registration failed.');
-        }
-    });
+// export const registerCommand = new Command()
+//     .name('register')
+//     .description('Register a new plugin with a URL')
+//     .argument('<url>', 'URL of the plugin')
+//     .action(async (url) => {
+//         const pluginId = new URL(url).hostname;
+//         const accountId = await getAccountId(url)
+//         const result = await registerPlugin(pluginId, accountId);
+//         if (result) {
+//             const receivedId = await openPlayground(result);
+//             console.log(`Received ID from playground: ${receivedId}`);
+//         } else {
+//             console.log('Registration failed.');
+//         }
+//     });
