f53753117b40a988da19093e0daee78cef171c2b
Markeljan Sokoli <Markeljan19@gmail.com>
2025-02-12 10:05:35 -0500

    revert default runtime url

--- src/commands/dev.ts
+++ src/commands/dev.ts
@@ -37,9 +37,7 @@ async function findAvailablePort(startPort: number): Promise<number> {
 
 const API_CONFIG: ApiConfig = {
   key: process.env.BITTE_API_KEY!,
-  url:
-    process.env.BITTE_API_URL ||
-    "https://ai-runtime-446257178793.europe-west1.run.app",
+  url: process.env.BITTE_API_URL || "https://wallet.bitte.ai/api/v1/chat",
   serverPort: DEFAULT_PORT,
 };
 
