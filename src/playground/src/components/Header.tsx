68bb347f5f1cd915ceff3dfa507707b41c7efc35
Sérgio <sergioseco98@gmail.com>
2025-02-27 14:13:47 +0000

    Fix button alignment

--- src/playground/src/components/Header.tsx
+++ src/playground/src/components/Header.tsx
@@ -4,10 +4,8 @@ import { NearWalletConnector } from "./near/WalletConnector";
 export const Header = (): JSX.Element => {
   return (
     <header>
-      <div>
+      <div className="max-w-7xl mx-auto px-4 flex justify-end items-center gap-8">
         <EvmWalletConnector />
-      </div>
-      <div>
         <NearWalletConnector />
       </div>
     </header>
