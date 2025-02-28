2aa24b6b0e50adc0d6a2999b2c1745c5478100aa
Markeljan Sokoli <Markeljan19@gmail.com>
2025-02-27 17:52:19 -0700

    fix crypto polyfills

--- src/playground/vite.config.ts
+++ src/playground/vite.config.ts
@@ -1,9 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), nodePolyfills()],
   define: {
     global: "globalThis",
     "process.env": {},
